bacd13c2d59b372718e05e1c3e81cb54eed3177a
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-18 21:20:24 -0300

    Code improvement

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ class Department:
 
 
 class Employee:
-    def __init__(self, code, name, salary):
+    def __init__(self, code, name, salary, department):
         if (type(self) == Employee):
             raise TypeError('Employee não pode ser instanciado diretamente')
         else:
+            self.__departament = department
             self.code = code
             self.name = name
             self.salary = salary
@@ -21,33 +22,25 @@ class Employee:
     def get_hours(self):
         return 8
 
-
-class Manager(Employee):
-    def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department('managers', 1)
-
     def get_departament(self):
         return self.__departament.name
 
-    def set_department(self, department):
-        self.__departament.name = department
+    def set_department(self, department_name):
+        self.__departament.name = department_name
+
+
+class Manager(Employee):
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary, Department('managers', 1))
 
     def calc_bonus(self):
         return self.salary * 0.15
 
 
-class Seller(Manager):
+class Seller(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department('sellers', 2)
+        super().__init__(code, name, salary, Department('sellers', 2))
         self.__sales = 0
-
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_department(self, department):
-        self.__departament.name = department
 
     def get_sales(self):
         return self.__sales
