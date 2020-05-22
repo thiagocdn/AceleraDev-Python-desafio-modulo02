f5ce84b4129c596cff86436fad2370b8c6a497e1
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-21 23:12:31 -0300

    code adjustments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 
 
 class Department:
@@ -6,27 +7,25 @@ class Department:
         self.code = code
 
 
-class Employee:
+class Employee(ABC):
     def __init__(self, code, name, salary, department):
-        if (type(self) == Employee):
-            raise TypeError('Employee não pode ser instanciado diretamente')
-        else:
-            self.__departament = department
-            self.code = code
-            self.name = name
-            self.salary = salary
+        self.__department = department
+        self.code = code
+        self.name = name
+        self.salary = salary
 
+    def get_departament(self):
+        return self.__department.name
+
+    def set_department(self, new_department):
+        self.__department.name = new_department
+
+    @abstractmethod
     def calc_bonus(self):
-        raise NotImplementedError('Necessário implementação da função')
+        pass
 
     def get_hours(self):
         return 8
-
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_department(self, department_name):
-        self.__departament.name = department_name
 
 
 class Manager(Employee):
@@ -42,11 +41,11 @@ class Seller(Employee):
         super().__init__(code, name, salary, Department('sellers', 2))
         self.__sales = 0
 
+    def calc_bonus(self):
+        return self.__sales * 0.15
+
     def get_sales(self):
         return self.__sales
 
-    def put_sales(self, value):
-        self.__sales += value
-
-    def calc_bonus(self):
-        return self.__sales * 0.15
+    def put_sales(self, sales):
+        self.__sales += sales
